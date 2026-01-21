e09f0afe50e65ae559d40e6fdaa9f90c8fcdd515
Lex Ferguson <82519402+SynisterSage@users.noreply.github.com>
2026-01-20 20:48:36 -0500

    config for render 2

--- backend/config.js
+++ backend/config.js
@@ -1,16 +1,15 @@
 /* eslint-disable n/no-process-env */
 
+const fs = require('fs');
 const path = require('path');
 const dotenv = require('dotenv');
 const moduleAlias = require('module-alias');
 
 const NODE_ENV = process.env.NODE_ENV ?? 'development';
 
-const result2 = dotenv.config({
-  path: path.join(__dirname, `./config/.env.${NODE_ENV}`),
-});
-
-if (result2.error) {
+const envPath = path.join(__dirname, `./config/.env.${NODE_ENV}`);
+const result2 = fs.existsSync(envPath) ? dotenv.config({ path: envPath }) : null;
+if (result2?.error) {
   throw result2.error;
 }
 
